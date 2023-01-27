375de96ebdbd4dd01a7d6692be02825892e9cf64
mikhail_malkov <mikhailmalkov9@gmail.com>
2023-01-27 17:52:29 -0600

    created exercise2

---
+++ exercise2.py
@@ -0,0 +1,10 @@
+# Задача 2: Найдите сумму цифр трехзначного числа.
+# 123 -> 6 (1 + 2 + 3)
+# 100 -> 1 (1 + 0 + 0)
+
+print("put 3 digit number: ")
+a = int(input())
+b = int(input())
+c = int(input())
+print("result: ")
+print(a+b+c)
